7149bbc1f9c8c9853b0ad6f2f36bdabd28d70cc9
vytorrennan <crosdeadbr@gmail.com>
2024-02-11 20:53:41 +0000

    Change in the gitignore file

---
+++ src/main/java/com/pacote/ecommerce/entities/User.java
@@ -0,0 +1,77 @@
+package com.pacote.ecommerce.entities;
+
+import java.time.LocalDate;
+
+/**
+ * User
+ */
+public class User {
+
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(long id, String name, String email, String phone, LocalDate birthDate, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
