f7afedb3175a332f354da9433ad8e314f480fc27
vytorrennan <crosdeadbr@gmail.com>
2024-02-12 11:35:51 +0000

    Criado tabela User

--- src/main/java/com/pacote/ecommerce/entities/User.java
+++ src/main/java/com/pacote/ecommerce/entities/User.java
@@ -2,11 +2,21 @@ package com.pacote.ecommerce.entities;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /**
  * User
  */
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
