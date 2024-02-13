0b36931c48011270d202aee069011051a64ade37
vytorrennan <crosdeadbr@gmail.com>
2024-02-13 20:04:26 +0000

    Adicionado tabelas product e category e linkado elas

--- src/main/java/com/pacote/ecommerce/entities/User.java
+++ src/main/java/com/pacote/ecommerce/entities/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,7 +24,10 @@ public class User {
     private long id;
 
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String phone;
     private LocalDate birthDate;
     private String password;
